abbad14114ca03896d0783c6e1e37a24bda63152
Pamela Cinthya Torres Gutiérrez <pamelatgtz@gmail.com>
2020-01-14 18:34:03 -0600

    Modificando prototipos

---
+++ clases/js/clase22.js
@@ -0,0 +1,35 @@
+//Modificando un prototipo  en JS
+//ejemplo crear un prototipo de persona mediante definir una funcion
+
+//1.-
+function Persona(nombre,apellido, altura) {  
+  //3.-
+  this.nombre = nombre
+  this.apellido = apellido
+  this.altura = altura
+}
+
+//NOTA: todas las funciones se deben definir juntas
+//2.-
+Persona.prototype.saludar = () => {
+  console.log(`Hola, me llamo ${this.nombre} ${this.aplello}`)
+}
+//5.-
+Persona.prototype.soyAlto = () => {
+    return this.altura > 1.8
+}
+
+//3.-
+let pame = new Persona('Pamela', 'Torres', 1.60)
+let sacha = new Persona('Erika', 'Lifszyc', 1.85)
+let erika = new Persona('Erika', 'Luna', 1.75)
+
+//4.-
+pame.saludar()
+pame.soyAlto()
+sacha.saludar()
+sacha.soyAlto()
+erika.saludar()
+erika.soyAlto()
+
+
